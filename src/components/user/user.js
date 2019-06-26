99f49f73beb0abbe46a523483382bb98f098c065
ArcFpzzW <694025826@qq.com>
2019-06-26 10:18:37 +0800

    user Modify layout

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -10,7 +10,7 @@ import './user.css'
 		render(){
 			    console.log(this.props)
 				return <div>
-						user<div className="user_Box">
+						<div className="user_Box">
 								<div className="text_Box">
 									<h3 className="tit_Wb">[放飞梦想]</h3>
 									<p className="tit_Wb">很难说什么事办不到的事情，因为昨天的梦想可以使今天的希望，并且还可以成为明天的现实。</p>
@@ -24,11 +24,49 @@ import './user.css'
 									</div>
 								</div>
 								<div className="list_Box">
+									<div className="luck_Box">
+										<div className="star_Box">
+											<span>今日份星座运势</span>
+										</div>
+										<div className="alma_Box">
+											<span>今日份黄历运势</span>
+										</div>
+
+									</div>
 									<ul className="list_Ul_Box">
-										<li className="con_Li"> joker收藏</li>
+										<li className="con_Li">
+											<img src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561522659121&di=d52545a9181f065e90cf2b031e830f23&imgtype=0&src=http%3A%2F%2Fimages.669pic.com%2Felement_pic%2F95%2F66%2F62%2F97%2Fd8f0c8a2eb94d6402b0121e37607f081.jpg' />
+											 <span>joker收藏</span>
+											 <i> </i>
+										</li>
+										<li>
+											
+											<img src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561524328444&di=b3e5b1475a52eafef8fe5815830fce45&imgtype=0&src=http%3A%2F%2Fpic37.nipic.com%2F20140116%2F2531170_115618181000_2.jpg' />
+											 <span>纸飞机专用勋章墙</span>
+											 <i> </i>
 
+										</li>
+										<li>
+										
+											<img src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561524748880&di=4ef8c9c451e538c9e8039777801fa3b8&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F02%2F30%2F89%2F59c0788305336_610.jpg' />
+											 <span>组合红包</span>
+											 <i> </i>
 
+										</li>
+										<li>
+				
+											<img src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561524886431&di=e2c2dd0f72df98be089534c6d8cbb7c3&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F00%2F76%2F26%2F58b8886e01297_610.jpg' />
+											 <span>纸飞机神秘客服</span>
+											 <i> </i>
 
+										</li>
+										<li>
+				
+											<img src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561525210694&di=04129cad542fbcd14cb2bdcb22239fbb&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F18%2F08%2F07%2F17b99157dc4cff1fd7b9327235fde4ad.jpg%2521%2Ffwfh%2F804x804%2Fquality%2F90%2Funsharp%2Ftrue%2Fcompress%2Ftrue' />
+											 <span>纸飞机个人设置</span>
+											 <i> </i>
+
+										</li>
 									</ul>
 
 
