d8c2eb2630a39e5e777b24b25011137f7f2105c6
Lmeuck <2632946122@qq.com>
2019-06-26 11:18:46 +0800

    peiru

--- src/components/second/second.js
+++ src/components/second/second.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react'
 import Footer from '../common/footer'
 import {connect} from 'react-redux';
 import actionlist from './actionCreator'
+import './second.css'
  class Second extends Component {
 	componentDidMount(){
 		this.props.getData();
@@ -9,6 +10,24 @@ import actionlist from './actionCreator'
 		render(){
 			    console.log(this.props.list)
 				return <div>
+				       <div className="second_x_box">
+				            <ul>
+                                {
+                                	this.props.list.map((item,index)=>{
+                                		return <li key={index} >
+                                		     <div className="second_li_box">
+                                                 <h1>{item.title}</h1>
+                                                 <p>{item.text}</p>
+                                                 <span>{item.ct}</span>
+                                                 <b>收藏</b>
+                                                 <i className="fa fa-star-o"></i>
+                                		     </div>
+
+                                		</li>
+                                	})
+                                }
+				            </ul>
+				       </div>
 						second
 						<Footer/>
 					   </div>
