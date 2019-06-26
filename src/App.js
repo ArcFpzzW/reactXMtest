4b0196446ca69ff4cb4e0f8b956badea56e2ae05
“ <930950646@qq.com>
2019-06-26 14:24:11 +0800

    成型

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import Third from './components/third/third'
 import User from './components/user/user'
 import Luck from './components/user/Luck'
 import './App.css';
-import {BrowserRouter as Router,Route,Redirect } from 'react-router-dom'
+import {BrowserRouter as Router,Route,Redirect,Switch } from 'react-router-dom'
 function App() {
   return (
     <Router>
       <div className="App">
-      {/* <Redirect from="/" to="/home" exact /> */}
-         <Route path="/home" component={Home} />
-         <Route path="/second" component={Second} />
-         <Route path="/third" component={Third} />
-         <Route path="/user" component={User} />
-         <Route path="/luck" component={Luck} />
-         <Redirect from="/" to="/home" exact />
+        <Switch>
+         <Route path="/home" component={Home}  exact/>
+         <Route path="/second" component={Second} exact />
+         <Route path="/third" component={Third}  exact/>
+         <Route path="/user" component={User}  exact/>
+         <Route path="/luck" component={Luck}  exact/>
+         <Route path="/" component={Home}  exact/>
+         </Switch>
       </div>
     </Router>
   );
--- src/components/third/third.js
+++ src/components/third/third.js
@@ -6,9 +6,23 @@ class Car extends Component {
 	render(){
 			console.log(this.props)
 			return <div><ul>
-					  {
+					  
+						{
+							this.props.list.map((item,index)=>{
+								return <li key={index} >
+									<div className="second_li_box">
+										<h1>{item.title}</h1>
+										<p>{item.text}</p>
+										<span>{item.ct}</span>
 
-					  }
+										<i className="fa fa-star-o"></i>
+										<button> 取消收藏</button>
+									</div>
+
+								</li>
+							})
+						}
+					  
 				   </ul>
 					
 					   <Footer />
@@ -35,13 +49,6 @@ var mapState=(state)=>{
 }
 var mapDispatch=(dispatch)=>{
 return {
-	inc(id){
-		
-		dispatch(actions.incAction(id));
-	},
-	dec(id){
-		dispatch(actions.decAction(id));
-	},
 	change(id){
 		dispatch(actions.changeAction(id));
 	}
